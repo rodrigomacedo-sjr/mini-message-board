50925611704cd895f81d8286cf88bf989b199d39
rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>
2026-07-11 16:56:13 -0300

    feat: html skeleton and mock db
    Signed-off-by: rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>

---
+++ src/db.js
@@ -0,0 +1,20 @@
+const data = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+const getAll = () => {
+  return data;
+};
+
+export default {
+  getAll,
+};
